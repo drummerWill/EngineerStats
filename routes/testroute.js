5fcfa40b072dd45bbb083cf16fe03f8895a5ab15
drummerWill <53499870+drummerWill@users.noreply.github.com>
2021-12-16 22:10:10 -0600

    Update testroute.js

--- routes/testroute.js
+++ routes/testroute.js
@@ -1,4 +1,8 @@
 var request = require('request'); // "Request" library
+const fs = require('fs');
+
+var engineers = require('../engineers.json')
+console.log(engineers)
 
 module.exports = function(app){
     app.get('/test', function(req, res){
