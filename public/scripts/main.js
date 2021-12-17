9b2f4191a9d46c1dc6c36c5b970fed3f83d5c063
drummerWill <53499870+drummerWill@users.noreply.github.com>
2021-12-16 23:25:38 -0600

    vue issues

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,12 +4,26 @@ var app = new Vue({
     el: '#app',
     data () {
       return {
+        showCount: false,
+        showHello: true,
+        showImage: false,
+        count: 0
+      }
+    },
+    methods:{
+      changeToImage(){
+        this.showImage = true;
+        this.showCount = false;
+        this.showHello = false;
       }
     },
     mounted () {
+      debugger;
+      let self = this
       axios.get('/test')
         .then(response => {
             console.log(response)
         });
     },
   })
+
